64d8ac614ca240424bdc68f04906c137838729c3
Sourov Chowdhury <schowdhury@awesomemotive.com>
2024-10-19 22:57:57 +0600

    Add category search

--- includes/Ajax.php
+++ includes/Ajax.php
@@ -11,24 +11,73 @@ class Ajax {
 	public function __construct() {
 		add_action( 'wp_ajax_ws_filter_get_search_items', array( $this, 'get_search_items' ) );
 		add_action( 'wp_ajax_nopriv_ws_filter_get_search_items', array( $this, 'get_search_items' ) );
+
+		add_action( 'wp_ajax_ws_filter_get_categories', array( $this, 'get_categories' ) );
+		add_action( 'wp_ajax_nopriv_ws_filter_get_categories', array( $this, 'get_categories' ) );
 	}
 
 	public function get_search_items() {
-		$query = new WC_Product_Query( array(
-			'limit' => 10,
-		) );
+		$search = '';
+		$cat_id = '';
+
+		if ( isset( $_GET['search'] ) ) {
+			$search = sanitize_text_field( wp_unslash( $_GET['search'] ) );
+		}
+		if ( isset( $_GET['cat_id'] ) ) {
+			$cat_id = absint( wp_unslash( $_GET['cat_id'] ) );
+		}
+
+		$args = array(
+			'limit' => 9,
+		);
+
+		if ( ! empty( $search ) ) {
+			$args['s'] = $search;
+		}
+
+		if ( ! empty( $cat_id ) ) {
+			$args['tax_query'] = array(
+				array(
+					'taxonomy' => 'product_cat',
+					'field' => 'term_id',
+					'terms' => $cat_id,
+				),
+			);
+		}
+
+		$query = new WC_Product_Query( $args );
 
 		$products = array();
 
 		foreach ( $query->get_products() as $product ) {
 			$products[] = array(
-				'id' => $product->get_id(),
-				'name' => $product->get_name(),
+				'id'        => $product->get_id(),
+				'name'      => $product->get_name(),
+				'image'     => $product->get_image(),
+				'permalink' => $product->get_permalink(),
 			);
 		}
 
 		wp_send_json_success(
 			$products
 		);
+	}
+
+	public function get_categories() {
+		$terms = get_terms( array(
+			'taxonomy'   => 'product_cat',
+			'hide_empty' => true,
+		) );
+
+		$items = array();
+
+		foreach ( $terms as $term ) {
+			$items[] = array(
+				'id' => $term->term_id,
+				'name' => $term->name,
+			);
+		}
+
+		wp_send_json( $items );
 	}
 }
--- includes/templates/search-filter.php
+++ includes/templates/search-filter.php
@@ -1,30 +1,89 @@
-<div class="ws-filter-form" x-data>
-	<input type="text">
-	<button x-on:click="alert('Hello World!')">Search</button>
-</div>
+<section class="ws-filter">
+	<aside class="ws-categories">
+		<ul x-data>
+			<template x-for="catItem in $store.wsFilter.categories">
+				<li><a href="#" x-text="catItem.name" @click="(e) => $store.wsFilter.onCategorySearch(e, catItem.id)"></a></li>
+			</template>
+		</ul>
+	</aside>
+	<div class="ws-form-and-items">
+		<template x-data x-if="$store.wsFilter.loading" x-transition>
+			<div class="ws-loader">
+				<div class="ws-spin-loader"></div>
+			</div>
+		</template>
 
-<div class="ws-filter-items" x-data>
-	<template x-for="item in $store.wsFilter.data">
-		<div class="ws-filter-item">
-			<h3 x-html="item.name"></h3>
+		<div class="ws-filter-form" x-data>
+			<input type="text" @keyup="(e) => $store.wsFilter.onSearchInput(e)" x-model="$store.wsFilter.searchInput">
+			<button x-on:click="alert('Hello World!')">Search</button>
 		</div>
-	</template>
-</div>
+
+		<div class="ws-filter-items" x-data>
+			<template x-for="item in $store.wsFilter.data">
+				<div class="ws-filter-item">
+					<a x-html="item.image" x-bind:href="item.permalink"></a>
+					<h3>
+						<a x-bind:href="item.permalink" x-html="item.name"></a>
+					</h3>
+				</div>
+			</template>
+		</div>
+	</div>
+</section>
+
+
 
 <script>
 	document.addEventListener('alpine:init', () => {
 		Alpine.store('wsFilter', {
+            data: [],
+            categories: [],
+			searchInput: "",
+			searchTimer: null,
+			searchCatItemId: "",
+			loading: false,
 			init() {
+				this.getCategories();
 				this.getItems();
 			},
-            data: [],
             getItems() {
+				this.loading = true;
                 let url = WS_Filter.ajax_url + "?action=ws_filter_get_search_items";
+				url += '&search=' + this.searchInput;
+				url += '&cat_id=' + this.searchCatItemId;
 
 				fetch( url ).then((response) => response.json()).then((response) => {
 					this.data = response.data;
+					this.loading = false;
 				});
-            }
+            },
+			getCategories() {
+				let url = WS_Filter.ajax_url + "?action=ws_filter_get_categories";
+
+				fetch( url ).then((response) => response.json()).then((response) => {
+					this.categories = response;
+				});
+			},
+			onSearchInput(event) {
+				clearTimeout(this.searchTimer);
+
+				// Check the character length is more than or equal to 3.
+				let text = String( event.target.value );
+
+				if ( text.length < 3 ) {
+					return;
+				}
+
+				this.searchTimer = setTimeout(() => {
+					this.getItems();
+				}, 1000);
+			},
+			onCategorySearch(event, catItemId) {
+				event.preventDefault();
+
+				this.searchCatItemId = catItemId;
+				this.getItems();
+			}
         });
 	});
 </script>
